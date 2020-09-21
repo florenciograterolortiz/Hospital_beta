8469d7b907a3aa47b9f1e0fa5de857e507e80abe
Florencio <inggraterol76@gmail.com>
2020-09-21 11:58:06 -0400

    Primer Commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Prueba</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/styles.css">
+ <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+</head>
+<body>
+
+<div class="top-bar_Sup  hidden-xs ">
+    <div class="container">
+        <div class="gruop_items">
+            <img src="image/Icon awesome-facebook-f@2x.png">
+            <img src="image/Icon awesome-twitter@2x.png">
+            <img src="image/Icon awesome-linkedin-in@2x.png">
+        </div>
+    </div>
+</div>
+        <div class="top-bar hidden-xs">
+           <div class="container">
+               <div class="col-md-12">
+                            <a class="navbar-brand" href="./">
+                            <img src="image/urn_aaid_sc_US_b80ae776-9112-479e-b972-3b6bb0b7ea3f.png" alt="LOGO" class="img-responsive" data-height-percentage="73" data-actual-width="495" data-actual-height="224">
+                             </a>
+
+
+                            <div class="icon_contacts">
+
+                            <a><img src="image/Icon feather-phone@2x.png"></a>
+                               <div>
+                                <p class="top_content">
+                                    (55)5000-2222
+                                </p>
+                                <p class="top_content" >
+                                    (55)5000-2222
+                                </p>
+                                </div>
+
+
+                                  <a><img src="image/Icon_ionic.png"></a>
+                               <div>
+                                <p class="top_content">
+                                   Asesoria Lun-Vie 08:00-17:00
+                                </p>
+                                
+                                </div>
+
+                    <a><img src="image/Icon material-location-on@2x.png"></a>
+                               <div>
+                                <p class="top_content">
+                                Insurgentes Sur #1605, Piso 9
+                                </p class="top_content">
+                                <p class="top_content">
+                                   Col. San Jose Insurgentes,
+                                </p class="top_content">
+                                 <p class="top_content">
+                                    CP 03900, CDMX
+                                </p>
+                                </div>
+                            </div>
+                        </div>     
+                 </div>
+        </div>    
+        <!-- Navigation -->
+    <nav class="navbar navbar-inverse " role="navigation">
+        <div class="container">
+            
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="coulah"><a href="#" class="">Home </a></li>
+                    <li class="coulah"><a href="#" class="">Cotiza Ahora</a></li>
+                    <li class="coulah"><a href="#" class="">Que Hacer En Caso De Siniestro</a></li>
+                    <li class="coulah"><a href="#" class="">FAQS</a></li>
+                    <li class="coulah"><a href="#" class="">Contacto</a></li>
+                </ul>
+            </div>
+          
+        </div>
+    </nav>
+
+    <div class="page-content">
+       
+            <div id="sliderShow" class="swiper-container">
+               <div class="swiper-wrapper">
+                   <div class="swiper-slide swiper-slide" style="background-image: url(image/slider.png);height: 463px;background-repeat: no-repeat;">
+                    <br>
+                         <div class="swiper-slide-caption">
+                        <div class="swiper-content">
+                             <h2 class="titulo">GASTOS MÈDICOS MAYORES</h2>
+                             <br>
+                              <div class="content-slider">
+                                <p class="content-description">Antes de contratar un seguro, analiza tus necesidades y resuelve cualquier duda con REUNE agente de seguros y de finanzas</p>
+                                <br>
+                                <span class="fon-wech">Flex Plus</span>
+                                <br>
+                                <br>
+                                <span class="fon-wech" >¿Què caracteristicas tiene?</span>
+                                <ul></ul>
+                                <li class="content-description">Es un Seguro Individual o familiar que puede contratarse como su pricipal proteccion o como un complemento de una poliza de Gastos Medicos de prestacion laboral</li>
+                                <li class="content-description">Coberturas que consideran los avances mèdicos màs recientes como: reposicion de marcapasos., gastos del donador en caso de transplante, tratamientos co cèlulas madre, cirugia bariatrica entre otros.</li> 
+                                </ul>              
+                              </div>
+                            </div>
+                          </div>
+                         
+                          </div>
+                        </div>
+                    </div>
+                  </div>         
+       
+       
+<section id="Servicios" >
+    <div class="container">
+        <div class="col-md-12">
+            <br><br>
+    <div class="col-md-3 col-sm-3">
+        <figure class="content">
+              <img alt="boarding-plaster" src="image/036-hospital.png"> 
+        </figure>
+   
+        <div class="panel-body">
+            <p>Gastos</p>
+            <p>Hospitalitarios</p>
+
+        </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/001-syrup@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Medicamentos</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/005-physical therapy@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Rehabilitacion</p>
+
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+               
+   
+                <div class="panel-body">
+                   <p class="text_invers">TE CUBRIMOS ESTO</p>
+                    </div>
+
+                 <figure class="img_text_invers" >
+                    <img alt="boarding-plaster" src="image/noun_Arrow_ISQ.png"> 
+                </figure>
+            </div>
+
+
+       </div>
+    
+                  <div class="col-md-12">
+                    <br><br>
+            <div class="col-md-3 col-sm-3">
+        <figure class="content">
+              <img alt="boarding-plaster" src="image/015-medical report@2x.png"> 
+        </figure>
+   
+        <div class="panel-body">
+            <p>Honorarios Mèdicos</p>
+
+        </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/028-doctor@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Consulta y Orientacion Medica las 24 hrs de dia</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/050-ambulance@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Ambulancia</p>
+
+                </div>
+            </div>
+          
+
+       </div>
+
+<div class="col-md-12">
+
+          <br><br>
+            <div class="col-md-3 col-sm-3">
+       
+   
+        <div class="panel-body text incl">
+           <p class="text_incl">CUANDO ESTO PASE</p>
+
+        </div>
+
+         <figure class="figu_img_arrow">
+              <img alt="boarding-plaster" src="image/noun_Arrow_47775@2x.png">
+        </figure>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/027-fever@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Enfermedades</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/042-patient@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>Accidentes</p>
+
+                </div>
+            </div>
+            
+</div>
+
+<div class="botom">
+    <button type="button" class=" btn-primary btn-lg">Large button</button><button type="button" class=" btn-primary btn-lg">Large button</button><button type="button" class=" btn-primary btn-lg">Large button</button>
+  </div>
+
+ <div class="col-md-12">
+    <br><br>
+
+    <div class="questions">
+        <h3>¿PARA QUIEN ES EL SEGURO?</h3>
+    </div>
+    <div class="col-md-3 col-sm-3">
+        <figure class="content">
+              <img alt="boarding-plaster" src="image/couple@2x.png"> 
+        </figure>
+   
+        <div class="panel-body">
+            <p>PARA MI</p>
+
+        </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <figure class="content">
+                    <img alt="boarding-plaster" src="image/family@2x.png"> 
+                </figure>
+   
+                <div class="panel-body">
+                    <p>PARA MI FAMILIA</p>
+
+                </div>
+            </div>
+
+
+       </div>
+    
+
+</div>
+            
+</section>
+</body>
+</html>    
